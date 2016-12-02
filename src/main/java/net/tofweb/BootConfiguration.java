d30b743b24bc141b37abff8b0989562e112dcdfe
Lynn <Lynn@WithMushLove>
2016-12-01 19:16:45 -0600

    Added spring boot

---
+++ src/main/java/net/tofweb/BootConfiguration.java
@@ -0,0 +1,13 @@
+package net.tofweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootConfiguration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootConfiguration.class, args);
+	}
+
+}
